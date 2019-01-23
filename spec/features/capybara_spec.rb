41fa8fe791bb68f206a466c937114aa633fef45b
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-23 15:51:56 +0000

    Add test to see player 2's hitpoints

--- spec/features/capybara_spec.rb
+++ spec/features/capybara_spec.rb
@@ -7,4 +7,12 @@ feature 'Capybara tests...' do
     click_button 'submit'
     expect(page).to have_content 'Nikhil Will'
   end
+  
+  scenario "Player 1 can see Player 2's hitpoints" do
+    visit('/')
+    fill_in 'player1', with: 'Nikhil'
+    fill_in 'player2', with: 'Will'
+    click_button 'submit'
+    expect(page).to have_content 'Hitpoints: 23'
+  end
 end
