78fa19fdbb7da770aa202c9f1344e139b2d64da9
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-23 15:53:16 +0000

    Modify /names and /play route to add hitpoints variable

--- app.rb
+++ app.rb
@@ -1,15 +1,29 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  
+  enable :sessions
+
+
   get '/' do
     erb :index
   end
 
-  post '/name' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
-    erb :name
+  post '/names' do
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    session[:player2_hitpoints] = 23
+    redirect '/play'
   end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    @player2_hitpoints = session[:player2_hitpoints]
+    erb :play
+  end
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
