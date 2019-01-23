473da5196d909c5b760e261b7f349b4b4fcce470
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-23 16:04:18 +0000

    Modify capybara_spec

--- spec/features/capybara_spec.rb
+++ spec/features/capybara_spec.rb
@@ -1,18 +1,12 @@
 
 feature 'Capybara tests...' do
   scenario 'Players can see their submitted names on the screen' do
-    visit('/')
-    fill_in 'player1', with: 'Nikhil'
-    fill_in 'player2', with: 'Will'
-    click_button 'submit'
+    sign_in_and_play
     expect(page).to have_content 'Nikhil Will'
   end
   
   scenario "Player 1 can see Player 2's hitpoints" do
-    visit('/')
-    fill_in 'player1', with: 'Nikhil'
-    fill_in 'player2', with: 'Will'
-    click_button 'submit'
+    sign_in_and_play
     expect(page).to have_content 'Hitpoints: 23'
   end
 end
