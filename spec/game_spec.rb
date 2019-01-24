81c5fffd4414f3c59ac34aca1968b92d404d001a
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-24 16:13:05 +0000

    Add tests for '#attack'

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,15 @@
 require 'game'
 
 RSpec.describe Game do
-  
+  subject(:game) { described_class.new }
+  let(:player1) { double :player }
+  let(:player2) { double :player }
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
+    end
+  end
+
 end
